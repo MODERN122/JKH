c900b269ce7f3000299567d654f181fda0adca8f
Anton <shestakovicha>
2019-07-21 16:50:39 +0500

    в разработке

--- JKH/urls.py
+++ JKH/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('uk_lk/', include('uk_lk.urls')),
     path('login/', login_page, name='login_page'),
     path('logout/', logout_page, name='logout_page'),
+    path('in_developing/', TemplateView.as_view(template_name='in_developing.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
